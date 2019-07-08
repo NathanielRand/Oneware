86f735d6f22e80777d449d487e80329f589ac682
Nathaniel Rand <nathanielrand@Nathaniels-MacBook-Air.local>
2019-07-08 18:44:21 -0400

    Add logo to home page; Finish about page

--- main.go
+++ main.go
@@ -8,15 +8,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View
+var aboutView *views.View
 var contactView *views.View
-var servicesView *views.View
+var homeView *views.View
+var partnersView *views.View
 var projectsView *views.View
+var servicesView *views.View
 
-func home(w http.ResponseWriter, r *http.Request) {
+func about(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeView.Template.ExecuteTemplate(w,
-		homeView.Layout, nil); err != nil {
+	if err := aboutView.Template.ExecuteTemplate(w,
+		aboutView.Layout, nil); err != nil {
 		panic(err)
 	}
 }
@@ -29,10 +31,18 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func services(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := servicesView.Template.ExecuteTemplate(w,
-		servicesView.Layout, nil); err != nil {
+	if err := homeView.Template.ExecuteTemplate(w,
+		homeView.Layout, nil); err != nil {
+		panic(err)
+	}
+}
+
+func partners(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := partnersView.Template.ExecuteTemplate(w,
+		partnersView.Layout, nil); err != nil {
 		panic(err)
 	}
 }
@@ -45,11 +55,21 @@ func projects(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func services(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := servicesView.Template.ExecuteTemplate(w,
+		servicesView.Layout, nil); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	homeView = views.NewView("materialize", "views/home.gohtml")
+	aboutView = views.NewView("materialize", "views/about.gohtml")
 	contactView = views.NewView("materialize", "views/contact.gohtml")
-	servicesView = views.NewView("materialize", "views/services.gohtml")
+	homeView = views.NewView("materialize", "views/home.gohtml")
+	partnersView = views.NewView("materialize", "views/partners.gohtml")
 	projectsView = views.NewView("materialize", "views/projects.gohtml")
+	servicesView = views.NewView("materialize", "views/services.gohtml")
 
 	r := mux.NewRouter()
 
@@ -60,8 +80,10 @@ func main() {
 
 	// Static Routes
 	r.HandleFunc("/", home)
-	r.HandleFunc("/services", services)
-	r.HandleFunc("/projects", projects)
+	r.HandleFunc("/about", about)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/partners", partners)
+	r.HandleFunc("/projects", projects)
+	r.HandleFunc("/services", services)
 	http.ListenAndServe(":3000", r)
 }
